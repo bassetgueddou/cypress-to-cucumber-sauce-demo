ec3e7bed5b94a76db1346c874102e49479daf66b
bassetgueddou <bassetgueddou@gmail.com>
2025-02-18 15:47:39 +0100

    renaming steps folder and add steps

---
+++ cypress/steps/checkout.steps.js
@@ -0,0 +1,7 @@
+const { Given, When, Then } = require('cucumber');
+const { expect } = require('chai');
+
+
+Given('je suis sur la page {string}', (s) => {
+  // Write code here that turns the phrase above into concrete actions
+})
